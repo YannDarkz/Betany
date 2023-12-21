120e00de4b9cf2bfaaf8b5a493736646508102a3
YANNDARK <iansantosmcqueen@gmail.com>
2023-12-21 15:48:28 -0300

    inicial

--- scripts/kiss.js
+++ scripts/kiss.js
@@ -1,14 +1,21 @@
 const btnSim = document.getElementById('btnSim')
 const btnNao = document.getElementById('btnNao');
-const amorAny = document.querySelector('h1')
+const resp = document.getElementById('resp')
 let numeroCliques = 0;
+const usuario = document.getElementById('usuario')
+
+const nome = prompt('Qual seu nome?')
 
 
+usuario.innerHTML = `Olá ${nome} Vem aqui em ksa ver`
 btnSim.addEventListener('click', () => {
-amorAny.innerText = "TE AMU"
+resp.innerText =
+ `Não tenho nem conta netflix,
+  então ja sabe ne => METFLIXXX
+  ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)`
     setTimeout(() => {
-        window.open('https://www.youtube.com/watch?v=k1rKpUjiGgM&ab_channel=PianoeTecladoTutorial', '_blank');
-    }, 2000);
+        window.open('https://www.netflix.com/', '_blank');
+    }, 3000);
 })
 
 
